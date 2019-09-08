588fda0b66c1e6d9078851b73740a78e58b52efc
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-09-08 20:33:13 +0300

    Update Solution.py

--- P003-Shortest-Word/Solution.py
+++ P003-Shortest-Word/Solution.py
@@ -1,6 +1,6 @@
 def find_short(s):
-    words = s.split()
-    l = len(words[0])
+    words = s.split() #split() function on strings returns a list of words
+    l = len(words[0]) #We give shortest length the value of the first words so we can compare
     for word in words:
         if (len(word) < l):
             l = len(word)
