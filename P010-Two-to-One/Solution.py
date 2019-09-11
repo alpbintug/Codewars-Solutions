9c8546c43ce5aafe17b8bcaa4117e6d892b5243c
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-09-11 22:52:27 +0300

    Update Solution.py

--- P010-Two-to-One/Solution.py
+++ P010-Two-to-One/Solution.py
@@ -1,2 +1,2 @@
 def longest(s1, s2):
-    return ''.join(sorted(set(s1+s2)))
+    return ''.join(sorted(set(s1+s2))) # set function returns the distinct characters from an string, sorted function sorts it, ''.join part joins that value to an empty string and return it.
