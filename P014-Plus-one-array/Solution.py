e04882b91b18ebfc8d78480437af2a5eb57f182a
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-09-21 17:34:12 +0300

    Update Solution.py

--- P014-Plus-one-array/Solution.py
+++ P014-Plus-one-array/Solution.py
@@ -1,5 +1,5 @@
 def up_array(arr):
-    if(len(arr)==0):
+    if(len(arr)==0):#If array has 0 length, it needs to return None, if we don't check this, it will return 1
         return None
     val = 0
     lst = []
@@ -8,8 +8,8 @@ def up_array(arr):
             return None
         val += arr[-i-1]*10**i
     val += 1
-    while(val > 0):
+    while(val > 0): #Creating new array in a reversed order, because it is easier to do.
         lst.append(val%10)
         val//=10
-    return lst[::-1]
+    return lst[::-1] #Returning the reverse of our newly created reversed list so it is normal
     
